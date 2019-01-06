d5527891b768b737e9421d06a2ea31cc7c1de8d3
Kevin Wang <rocwang0316@gmail.com>
2019-01-05 16:39:53 -0800

    Added the unit test for the fetch-unattached-journal function.

---
+++ __tests__/unit_tests/fetch-unattached-journal.js
@@ -0,0 +1,39 @@
+require('../helpers/initailEnvsForUnitTest');
+const { ObjectId } = require('mongodb');
+const { handler } = require('../../functions/fetch-unattached-journal');
+
+const findReturnValue = { _id: 'testId' };
+const mockFind = jest.fn().mockReturnValue(findReturnValue);
+const mockCollection = jest.fn().mockReturnValue({ find: mockFind });
+
+jest.mock('../../middlewares/wrapper', () => functionHandler => functionHandler);
+jest.mock('../../libs/MongoDBHelper', () => ({
+  promiseNextResult: jest.fn().mockImplementation(cb => cb({ collection: mockCollection })),
+}));
+// jest.mock('../../libs/log', () => ({ error: jest.fn() }));
+jest.mock('../../libs/cloudwatch', () => ({ trackExecTime: jest.fn().mockImplementation((name, func) => func()) }));
+
+describe('fetch-unattached-journal', () => {
+  test('Verified user calls', async () => {
+    const event = { queryStringParameters: { journalId: '5c2be5a983e41424f9943add' } };
+    const context = {
+      user: { _id: 'id' },
+    };
+    const callback = jest.fn();
+    const mongodb = require('../../libs/MongoDBHelper');
+    const cloudwatch = require('../../libs/cloudwatch');
+
+    await handler(event, context, callback);
+
+    expect(cloudwatch.trackExecTime).toHaveBeenCalledTimes(1);
+    expect(mongodb.promiseNextResult).toHaveBeenCalledTimes(1);
+    expect(mockCollection).toHaveBeenCalledTimes(1);
+    expect(mockCollection).toHaveBeenLastCalledWith(process.env.journalEntriesCollectionName);
+    expect(mockFind).toHaveBeenCalledTimes(1);
+    expect(mockFind).toHaveBeenLastCalledWith(
+      { _id: new ObjectId(event.queryStringParameters.journalId), user_id: context.user._id },
+    );
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenLastCalledWith(null, { statusCode: 200, body: JSON.stringify(findReturnValue) });
+  });
+});
