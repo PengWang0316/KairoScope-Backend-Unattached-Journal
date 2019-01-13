4b9672b97d7514fefe5153921655ad402350ecde
Kevin Wang <rocwang0316@gmail.com>
2019-01-12 16:31:47 -0800

    Added the unit test for the create-journal function.

---
+++ __tests__/unit_tests/create-journal.js
@@ -0,0 +1,74 @@
+import { ObjectId } from 'mongodb';
+
+import mongodbHelper from '../../libs/MongoDBHelper';
+import cloudwatch from '../../libs/cloudwatch';
+import log from '../../libs/log';
+import { handler } from '../../functions/create-journal';
+
+require('../helpers/initailEnvsForUnitTest');
+
+const mockInsertOne = jest.fn();
+const mockCollection = jest.fn().mockReturnValue({ insertOne: mockInsertOne });
+
+jest.mock('../../middlewares/wrapper', () => functionHandler => functionHandler);
+jest.mock('../../libs/MongoDBHelper', () => ({
+  promiseInsertResult: jest.fn().mockImplementation(cb => cb({ collection: mockCollection })),
+}));
+jest.mock('../../libs/log', () => ({ error: jest.fn() }));
+jest.mock('../../libs/cloudwatch', () => ({ trackExecTime: jest.fn().mockImplementation((name, func) => func()) }));
+jest.mock('mongodb', () => ({ ObjectId: jest.fn() }));
+
+describe('create-journal', () => {
+  beforeAll(() => {
+    mongodbHelper.promiseInsertResult.mockClear();
+    log.error.mockClear();
+    cloudwatch.trackExecTime.mockClear();
+  });
+
+  test('Verified user calls', async () => {
+    const event = { body: '{"journal":{"date":"01/12/2019", "readings": []}}' };
+    const context = {
+      user: { _id: 'id' },
+    };
+    const callback = jest.fn();
+
+    await handler(event, context, callback);
+
+    expect(cloudwatch.trackExecTime).toHaveBeenCalledTimes(1);
+    expect(mongodbHelper.promiseInsertResult).toHaveBeenCalledTimes(1);
+    expect(mockCollection).toHaveBeenCalledTimes(1);
+    expect(mockCollection).toHaveBeenLastCalledWith(process.env.journalEntriesCollectionName);
+    expect(mockInsertOne).toHaveBeenCalledTimes(1);
+    expect(mockInsertOne).toHaveBeenLastCalledWith(
+      { _id: new ObjectId(), user_id: context.user._id, date: new Date('01/12/2019') },
+    );
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenLastCalledWith(null, { statusCode: 200 });
+    expect(log.error).not.toHaveBeenCalled();
+  });
+
+  test('Verified user calls with an error', async () => {
+    const event = { body: '{"journal":{"date":"01/12/2019", "readings": []}}' };
+    const context = {
+      user: { _id: 'id' },
+      functionName: 'functionName',
+    };
+    const callback = jest.fn();
+    cloudwatch.trackExecTime.mockRejectedValueOnce('Error Message');
+
+    await handler(event, context, callback);
+
+    // expect(cloudwatch.trackExecTime).toHaveBeenCalledTimes(1);
+    // expect(mongodbHelper.promiseInsertResult).toHaveBeenCalledTimes(1);
+    // expect(mockCollection).toHaveBeenCalledTimes(1);
+    // expect(mockCollection).toHaveBeenLastCalledWith(process.env.journalEntriesCollectionName);
+    // expect(mockInsertOne).toHaveBeenCalledTimes(1);
+    // expect(mockInsertOne).toHaveBeenLastCalledWith(
+    //   { _id: new ObjectId(), user_id: context.user._id, date: new Date('01/12/2019') },
+    // );
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenLastCalledWith(null, { statusCode: 500 });
+    expect(log.error).toHaveBeenCalledTimes(1);
+    expect(log.error).toHaveBeenLastCalledWith(`${context.functionName} has an error message: Error Message`);
+  });
+});
