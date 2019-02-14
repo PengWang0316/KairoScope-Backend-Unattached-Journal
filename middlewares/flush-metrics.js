2586f8265229e3fa80e2381829f0ad4f2e184122
Kevin Wang <rocwang0316@gmail.com>
2019-02-13 20:11:15 -0800

    Removed middlewares from the project.

--- middlewares/flush-metrics.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const cloudwatch = require('@kevinwang0316/cloudwatch');
-
-// The middleware to flush the metrics to the CloudWatch
-module.exports = {
-  after: (handler, next) => cloudwatch.flush().then(() => next()),
-  onError: (handler, next) => cloudwatch.flush().then(() => next(handler.error)),
-};
--- middlewares/initialize-mongodb.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { initialConnects } = require('@kevinwang0316/mongodb-helper');
-
-module.exports = {
-  before: (handler, next) => {
-    initialConnects(handler.context.dbUrl, handler.context.dbName).then(() => next());
-  },
-};
